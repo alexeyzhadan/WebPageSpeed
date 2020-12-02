5504cc0e6bde8a047130b0b7bb47e266607e427c
Alexey Zhadan <alexeyzhadan.w@gmail.com>
2020-12-02 12:46:35 +0200

    add a controller and views for the main page

---
+++ WebPageSpeed/Controllers/MainController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPageSpeed.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string uri)
+        {
+            return View();
+        }
+    }
+}
--- WebPageSpeed/Startup.cs
+++ WebPageSpeed/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -26,10 +25,9 @@ namespace WebPageSpeed
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Main}/{action=Index}/{id?}");
             });
         }
     }
