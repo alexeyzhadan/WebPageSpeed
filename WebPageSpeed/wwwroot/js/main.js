5204f2114519380792105204228585d6987f25a0
Alexey Zhadan <alexeyzhadan.w@gmail.com>
2020-12-03 23:12:42 +0200

    add preloader

---
+++ WebPageSpeed/wwwroot/js/main.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    let elements = document.getElementsByClassName('load');
+
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('click', showLoader);
+    }
+}, false);
+
+function showLoader() {
+    document.getElementsByClassName('loader')[0].classList.add('loader--on');
+}
